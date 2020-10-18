97c69eb26f949be7c22dc4a704af7cfa82d836eb
Ben Lehmann <62218426+BenLehmann12@users.noreply.github.com>
2020-10-17 21:39:28 -0500

    Update test_set_membership.py

--- test_set_membership.py
+++ test_set_membership.py
@@ -4,7 +4,7 @@ from fun_with_collections import set_member as set
 
 class MyTestCase(unittest.TestCase):
     def test_something(self):
-        self.assertTrue(set.in_set({3,5,8,9}, 3))   #Should run True
+        self.assertTrue(set.in_set({3,5,8,9}, 5))   #Should run True
 
     def test_false(self):
         self.assertFalse(set.in_set({3,5,8,9}, 10))       #Should Run False
