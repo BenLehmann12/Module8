fcef6117c7883da32c8b23473c6dbfe8fdc5e30f
Ben Lehmann <62218426+BenLehmann12@users.noreply.github.com>
2020-10-19 13:06:05 -0500

    Update test_dict_membership.py

--- test_dict_membership.py
+++ test_dict_membership.py
@@ -1,9 +1,10 @@
 import unittest
+from fun_with_collections import dict_membership as dic
 
 
 class MyTestCase(unittest.TestCase):
     def test_something(self):
-        self.assertEqual(True, False)
+        self.assertEqual(dic.in_dict({3,6,9,12},3), True)
 
 
 if __name__ == '__main__':
